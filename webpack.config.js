9a442954b1cf2ca4450e9d7af1cf432f5ff00905
Oliver <oliver@therewillbeanimals.com>
2018-08-04 22:25:08 +0100

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,19 @@ module.exports = {
     publicPath: '/assets/scripts/',
     contentBase: path.join(__dirname, 'public'),
     port: 3000
-  }
+  },
 
+  module: {
+    rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }]
+  }
 };
