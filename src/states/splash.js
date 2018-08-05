68c6c08ce66cb47014409624610072f5218d9fe9
Oliver <oliver@therewillbeanimals.com>
2018-08-05 20:17:38 +0100

    Load cat

--- src/states/splash.js
+++ src/states/splash.js
@@ -13,11 +13,17 @@ export default class extends Phaser.State {
     //
     // load your assets
     //
+
+    // Load miner
     this.load.spritesheet('miner',
       'assets/images/sprites/miner.png',
       config.spriteSize,
       config.spriteSize
      );
+
+    // Load cat
+    this.load.spritesheet('cat_walk', 'assets/images/sprites/cat_walk.png', 18, 15);
+    this.load.spritesheet('cat_run', 'assets/images/sprites/cat_run.png', 20, 17);
   }
 
   create () {
